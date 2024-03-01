47bfadacc8b8f949d9ff85d6515fae0d38415701
K.S.Winodahewa <106149174+shehan1215@users.noreply.github.com>
2024-03-01 23:30:00 +0530

    Add files via upload

---
+++ student_sidebar.php
@@ -0,0 +1,13 @@
+<header class="header">
+        <a href="#">Home</a>
+        <div class="logout">
+            <a href="logout.php" class="btn btn-primary">Logout</a>
+        </div>
+    </header>
+    <aside>
+        <ul>
+            <li><a href="student_profile.php">My Profile</a></li>
+            <li><a href="courses.php">My Courses</a></li>
+            <li><a href="student_results.php">My Results</a></li>
+        </ul>
+    </aside>
